5e3610f79ebcda1a3e2ca8b744064be22e7d6f41
qct <quchentao@163.com>
2019-02-23 02:18:59 +0000

    Closes #16, clear apollo-env.properties and related dev.meta etc.

--- apollo-client/src/test/java/com/ctrip/framework/apollo/spring/boot/ApolloApplicationContextInitializerTest.java
+++ apollo-client/src/test/java/com/ctrip/framework/apollo/spring/boot/ApolloApplicationContextInitializerTest.java
@@ -1,6 +1,7 @@
 package com.ctrip.framework.apollo.spring.boot;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -24,7 +25,6 @@ public class ApolloApplicationContextInitializerTest {
     System.clearProperty("app.id");
     System.clearProperty(ConfigConsts.APOLLO_CLUSTER_KEY);
     System.clearProperty("apollo.cacheDir");
-    System.clearProperty(ConfigConsts.APOLLO_META_KEY);
   }
 
   @Test
@@ -32,21 +32,18 @@ public class ApolloApplicationContextInitializerTest {
     String someAppId = "someAppId";
     String someCluster = "someCluster";
     String someCacheDir = "someCacheDir";
-    String someApolloMeta = "someApolloMeta";
 
     ConfigurableEnvironment environment = mock(ConfigurableEnvironment.class);
 
     when(environment.getProperty("app.id")).thenReturn(someAppId);
     when(environment.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY)).thenReturn(someCluster);
     when(environment.getProperty("apollo.cacheDir")).thenReturn(someCacheDir);
-    when(environment.getProperty(ConfigConsts.APOLLO_META_KEY)).thenReturn(someApolloMeta);
 
     apolloApplicationContextInitializer.initializeSystemProperty(environment);
 
     assertEquals(someAppId, System.getProperty("app.id"));
     assertEquals(someCluster, System.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY));
     assertEquals(someCacheDir, System.getProperty("apollo.cacheDir"));
-    assertEquals(someApolloMeta, System.getProperty(ConfigConsts.APOLLO_META_KEY));
   }
 
   @Test
@@ -54,31 +51,26 @@ public class ApolloApplicationContextInitializerTest {
     String someAppId = "someAppId";
     String someCluster = "someCluster";
     String someCacheDir = "someCacheDir";
-    String someApolloMeta = "someApolloMeta";
 
     System.setProperty("app.id", someAppId);
     System.setProperty(ConfigConsts.APOLLO_CLUSTER_KEY, someCluster);
     System.setProperty("apollo.cacheDir", someCacheDir);
-    System.setProperty(ConfigConsts.APOLLO_META_KEY, someApolloMeta);
 
     String anotherAppId = "anotherAppId";
     String anotherCluster = "anotherCluster";
     String anotherCacheDir = "anotherCacheDir";
-    String anotherApolloMeta = "anotherApolloMeta";
 
     ConfigurableEnvironment environment = mock(ConfigurableEnvironment.class);
 
     when(environment.getProperty("app.id")).thenReturn(anotherAppId);
     when(environment.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY)).thenReturn(anotherCluster);
     when(environment.getProperty("apollo.cacheDir")).thenReturn(anotherCacheDir);
-    when(environment.getProperty(ConfigConsts.APOLLO_META_KEY)).thenReturn(anotherApolloMeta);
 
     apolloApplicationContextInitializer.initializeSystemProperty(environment);
 
     assertEquals(someAppId, System.getProperty("app.id"));
     assertEquals(someCluster, System.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY));
     assertEquals(someCacheDir, System.getProperty("apollo.cacheDir"));
-    assertEquals(someApolloMeta, System.getProperty(ConfigConsts.APOLLO_META_KEY));
   }
 
   @Test
@@ -90,6 +82,5 @@ public class ApolloApplicationContextInitializerTest {
     assertNull(System.getProperty("app.id"));
     assertNull(System.getProperty(ConfigConsts.APOLLO_CLUSTER_KEY));
     assertNull(System.getProperty("apollo.cacheDir"));
-    assertNull(System.getProperty(ConfigConsts.APOLLO_META_KEY));
   }
 }
--- apollo-core/src/main/java/com/ctrip/framework/apollo/core/ServiceNameConsts.java
+++ apollo-core/src/main/java/com/ctrip/framework/apollo/core/ServiceNameConsts.java
@@ -2,8 +2,6 @@ package com.ctrip.framework.apollo.core;
 
 public interface ServiceNameConsts {
 
-  String APOLLO_METASERVICE = "apollo-metaservice";
-
   String APOLLO_CONFIGSERVICE = "apollo-configservice";
 
   String APOLLO_ADMINSERVICE = "apollo-adminservice";
